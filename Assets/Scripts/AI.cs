5f8d77d2fb3801dc5576c4c3945994f7a27e474a
AIE\s162318 <mmatthewspatch1@gmail.com>
2018-04-23 15:12:20 +0930

    added basic AI script pls fix

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour {
+
+	public int health;
+	public Transform target;
+	public float minDis;
+	float speed;
+	public float minSpeed;
+	public float maxSpeed;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		speed = Random.Range (minSpeed, maxSpeed + 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = (transform.position + (target.position - transform.position).normalized * speed * Time.deltaTime);
+		transform.LookAt (target.position);
+		if (Vector3.Distance (transform.position, target.position) < minDis) {
+			Node targetNode = target.GetComponent<Node> ();
+			targetNode.CheckNextNode (GetComponent<AI>());
+		}
+
+	}
+
+	public void setTarget(Transform newTarget)
+	{
+		target = newTarget;
+	}
+
+	public void KillPlayer()
+	{
+		print ("He Ded");
+	}
+	public void setHealth()
+	{
+		health--;
+	}
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+	public Node[] potentialNodes;
+	public bool FinalNode;
+	// Use this for initialization
+	public void CheckNextNode(AI enemy)
+	{
+		if (!FinalNode) {
+			enemy.setTarget (potentialNodes[Random.Range(0, potentialNodes.Length)].transform);
+		} else 
+		{
+			enemy.KillPlayer ();
+		}
+	}
+}
